b4b39cc42f2b96208f4fdc9bdf66b87d76b3797c
nratliff2078 <72581364+nratliff2078@users.noreply.github.com>
2024-01-23 13:37:38 -0500

    Test

--- src/index.py
+++ src/index.py
@@ -2,4 +2,7 @@ import tornado.web
 
 class Handler(tornado.web.RequestHandler):
     def get(self):
-        self.write('<a href="/quote">Get a quote</a>')
+        self.write('<a href="/Profile/alice">Alice</a><br>')
+        self.write('<a href="/Profile/bob">Bob</a><br>')
+        self.write('<a href="/Profile/carol">Carol</a><br>')
+        self.write('<a href="/Profile/dave">Dave</a><br>')
